388f374792bd4a158c928397cf3b9ee2b8a3b841
Leonardo <leonardotandelicw@outlook.com>
2022-11-21 09:20:07 -0300

    initial code

---
+++ ByteBank/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using ByteBank;
+
+
+
+ContaCorrente contaDoLeonardo = new ContaCorrente(323, "21312321");
+
+Console.WriteLine("O Número da conta de Leonardo é: " + contaDoLeonardo.Conta);
+Console.WriteLine("Total de Contas: " + ContaCorrente.TotalDeContasCriadas);
+
+
+ContaCorrente contaDoLeonardo2 = new ContaCorrente(3233, "23232");
+
+Console.WriteLine("O Número da conta 2 de Leonardo é: " + contaDoLeonardo.Conta);
+Console.WriteLine("Total de Contas: " + ContaCorrente.TotalDeContasCriadas);
